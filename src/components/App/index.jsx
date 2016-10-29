82ef4ed8520e6b029799352896375be390c9d575
Elliot S Chi <elliotschi@gmail.com>
2016-10-28 22:22:31 -0700

    WIP: bar chart

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from '../../assets/logo.svg';
 import './styles.css'
-// import BarChart from '../BarChart'
+import BarChart from '../BarChart'
 import Visitors from '../Visitors'
 import DonutChart from '../DonutChart'
 import ProgressChart from '../ProgressChart'
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          {/*<BarChart />*/}
+          <BarChart />
           <Visitors />
           <div className="pad bottom-left-svg">
             <DonutChart padAngle={0.03}/>
--- src/components/BarChart/index.jsx
+++ src/components/BarChart/index.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import * as d3 from 'd3'
-import DataBars from '../DataBars'
 
-const BarChart = () => (
-  <div>Bar Chart...</div>
-)
+const DATA = [
+  { month:'Jan', value:40 },
+  { month:'Feb', value:50 },
+  { month:'Mar', value:65 },
+  { month:'Apr', value:60 },
+  { month:'May', value:70 },
+  { month:'Jun', value:55 },
+  { month:'Jul', value:80 },
+  { month:'Aug', value:55 },
+  { month:'Sep', value:75 },
+  { month:'Oct', value:50 },
+  { month:'Nov', value:60 },
+  { month:'Dec', value:75 }
+]
+
+const margins = {
+  top: 5,
+  right: 5,
+  bottom: 5,
+  left: 5
+}
+
+class BarChart extends React.Component {
+  static propTypes = {
+    width: PropTypes.number,
+    height: PropTypes.number,
+    chartId: PropTypes.string
+  }
+
+  static defaultProps = {
+    width: 300,
+    height: 70,
+    chartId: 'v_chart'
+  }
+
+  constructor() {
+    super()
+
+    this.state = {
+      width: 0
+    }
+  }
+
+  render() {
+    const { height, chartId } = this.props
+    const { width } = this.state
+    let w = width - margins.left + margins.right
+    let h = height - margins.top + margins.bottom
+
+    const transform = `translate(${margins.left},${margins.top})`
+
+    const xScale = d3.scaleOrdinal()
+                     .domain(DATA.map(({ month }) => month))
+                     .range([0, width])
+                    //  .round(0.35)
+
+    const yScale = d3.scaleLinear()
+                     .domain([0, 100])
+                     .range([height, 0])
+
+    const rectBackground = DATA.map(
+      (d, i) =>
+        <rect
+          fill="#58657f"
+          rx="3"
+          ry="3"
+          key={i}
+          x={xScale(d.month)}
+          y={margins.top - margins.bottom}
+          height={h}
+          width={xScale.rangeBand()}
+        /> 
+    )
+
+    const rectForeground = DATA.map(
+      (d, i) =>
+        <rect
+          fill="#74d3eb"
+          rx="3"
+          ry="3"
+          key={i}
+          x={xScale(d.month)}
+          y={yScale(d.value)}
+          className="shadow"
+          height={h- yScale(d.value)}
+          width={xScale.rangeBand()}/>
+    )
+
+    return (
+      <div>
+        <svg
+          id={chartId}
+          width={width}
+          height={height}
+        >
+          <g transform={transform}>
+            {rectBackground}
+            {rectForeground}
+          </g>
+        </svg>
+      </div>
+    )
+  }
+}
 
 export default BarChart
